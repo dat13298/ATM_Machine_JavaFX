90ac12e46ae928a5c4b6ef52c1621cada49a3c60
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-07-07 04:20:15 +0700

    config code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,13 @@
 module com.example.atm_machine {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires mysql.connector.java;
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires java.sql;
+        requires mysql.connector.java;
 
-
-    opens com.atm_machine to javafx.fxml;
-    exports com.atm_machine;
-    exports com.atm_machine.Entity;
-    opens com.atm_machine.Entity to javafx.fxml;
-    exports com.atm_machine.Service;
-    exports com.atm_machine.Connectivity;
-}
+        opens com.atm_machine to javafx.fxml;
+        exports com.atm_machine;
+        exports com.atm_machine.Entity;
+        opens com.atm_machine.Entity to javafx.fxml;
+        exports com.atm_machine.Service;
+        exports com.atm_machine.Connectivity;
+        }
